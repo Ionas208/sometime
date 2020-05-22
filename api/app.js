4864c6afa07b9b17815dfe0aa054be780baabe31
Jonas <seijod17@htl-kaindorf.at>
2020-05-22 17:20:57 +0200

    First commit

---
+++ api/app.js
@@ -0,0 +1,82 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
+var bodyParse = require('body-parser');
+const jwt = require('jsonwebtoken');
+const db = require('./queries');
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api/', apiRouter);
+app.get('/api/getUsers', authenticateToken, db.getAllUsers);
+app.post('/api/register', db.registerUser);
+app.post('/api/login', db.loginUser);
+app.post('/api/getTodo', authenticateToken, db.getTodosForUser);
+app.post('/api/createTodo', authenticateToken, db.createTodo);
+app.get('/api/getUsernameFromToken', authenticateToken, db.getUserName);
+
+app.use(cors());
+
+// catch 404 and forward to error handlers
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
+
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+function authenticateToken(req, res, next){
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if(token == null){
+    return res.sendStatus(401);
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if(err){
+      return res.sendStatus(403);
+    }
+    req.user = user;
+    next();
+  })
+}
+
+module.exports = app;
+
+
---
+++ api/queries.js
@@ -0,0 +1,132 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+var winston = require('winston');
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
+
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'sometimedb',
+    password: 'postgres',
+    port: 5432,
+});
+
+const getAllUsers = (req, res) => {
+    pool.query('SELECT * FROM sometimeuser', (error, result) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).json(result.rows);
+    })
+}
+
+const registerUser = (req, res) => {
+    const name = req.body.user;
+    const email = req.body.email;
+    const password = req.body.password;
+    bcrypt.hash(password, saltRounds, function (err, hash) {
+        if (err) {
+            winston.log("silly", err.message);
+            res.status(500).send("Error registering new user!");
+        }
+        else {
+
+            pool.query("INSERT INTO sometimeuser(username, email, password, role) VALUES ('{0}', '{1}', '{2}', 'user')".format(name, email, hash), (err) => {
+                if (err) {
+                    res.status(500).send("DB Error registering new user!");
+                }
+                else {
+                    res.status(200).send("Successfully registered!");
+                }
+            });
+        }
+    });
+}
+
+const loginUser = (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    pool.query("SELECT password, userid FROM sometimeuser WHERE email='" + email + "'", (err, result) => {
+        if (err) {
+            req.status(500).send("DB Error logging in!");
+        }
+        else {
+            if (result.rows[0] != null) {
+                const dbHash = result.rows[0].password;
+                const userid = result.rows[0].userid;
+                bcrypt.compare(password, dbHash, function (err, result) {
+                    if (err) {
+                        res.sendStatus(500);
+                    }
+                    if (result) {
+                        const user = { id: userid };
+                        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+                        res.status(200).json({ accessToken: accessToken });
+                    }
+                    else {
+                        res.sendStatus(400);
+                    }
+
+                })
+            }
+            else {
+                res.sendStatus(400);
+            }
+
+        }
+    });
+
+
+}
+
+const getTodosForUser = (req, res) => {
+    const userid = req.user.id;
+    pool.query("SELECT title, description FROM todoobject WHERE userid = " + userid, function (err, result) {
+        if (err) {
+            res.sendStatus(500)
+        }
+        else{
+            res.status(200).json(result.rows);
+        }
+        
+    })
+}
+
+const createTodo = (req, res) => {
+    const userid = req.user.id;
+    const title = req.body.title;
+    const description = req.body.description;
+    pool.query("INSERT INTO todoobject (userid, title, description) VALUES({0}, '{1}', '{2}')".format(userid, title, description), function (err, result){
+        if(err){
+            res.sendStatus(500);
+        }
+        else{
+            res.sendStatus(200);
+        }
+        
+    })
+}
+
+const getUserName = (req,res) =>{
+    const userid = req.user.id;
+    pool.query("SELECT username FROM sometimeuser WHERE userid = "+userid, function(err, result){
+        if(err){
+            res.sendStatus(500);
+        }
+        else{
+            res.status(200).json(result.rows);
+        }
+    })
+}
+
+String.prototype.format = function () {
+    var formatted = this;
+    for (var arg in arguments) {
+        formatted = formatted.replace("{" + arg + "}", arguments[arg]);
+    }
+    return formatted;
+};
+
+module.exports = { getAllUsers, registerUser, loginUser, getTodosForUser, createTodo, getUserName} 
